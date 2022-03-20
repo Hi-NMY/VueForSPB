6c914177aeed3e384e0a579b12c58eabd82b94e2
yaqin <1598440629@qq.com>
2022-03-20 15:02:59 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
     <el-button type="primary">主要按钮</el-button>
+    <span>{{msg}}</span>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data(){
+    return{
+      msg:"111"
+    }
+  }
 }
 </script>
 
